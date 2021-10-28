917b9e1b076be0ab55d8c8dc91817fdf4d483bd7
dmcleg <dmcleghorn@gmail.com>
2021-10-28 12:54:26 -0500

    commit

---
+++ server/server.js
@@ -0,0 +1,11 @@
+var path = require('path');
+
+var fs = require('fs');
+
+[
+    { name: "Drew", content: "dril tweets" },
+    { name: "Garrett", content: "heaven-piercing giga drill" },
+    { name: "Sam", content: "come see my show" },
+    { name: "Bug Bobbins", content: "lorem" },
+    { name: "JFSheff", content: "let's play valheim" }
+]
